733002f4b41cd92fa94ea16a999c38dcd89e2d0a
Michael Reed <myk27x@yahoo.com>
2015-08-27 13:45:50 -0400

    start with player name

--- roshambo.rb
+++ roshambo.rb
@@ -1,1 +1,8 @@
-#init commit
+def match_init
+  puts "Hurry, the match is starting!"
+  puts "But first... what is your name?"
+  name = gets.chomp
+  puts "Ok, #{name}, let's get started!"
+end
+
+match
