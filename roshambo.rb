3fdeace9e6e53191010c341db75dd4dd89ab7bd5
Michael Reed <myk27x@yahoo.com>
2015-08-27 13:52:34 -0400

    exits if no name

--- roshambo.rb
+++ roshambo.rb
@@ -1,8 +1,16 @@
+#  At the start of each match prompt for the name of the player
+
 def match_init
   puts "Hurry, the match is starting!"
   puts "But first... what is your name?"
   name = gets.chomp
-  puts "Ok, #{name}, let's get started!"
+#  If no name is given, the program should end
+  if name == ""
+    puts "WHOOPS!"
+    exit
+  else
+    puts "Ok, #{name}, let's get started!"
+  end
 end
 
-match
+match_init
