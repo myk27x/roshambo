da578ecf15a0335fc0d4932dee52ad508886ab47
Michael Reed <myk27x@yahoo.com>
2015-08-28 18:34:18 -0400

    added message-- flair needs fixed

--- roshambo.rb
+++ roshambo.rb
@@ -1,8 +1,8 @@
 class Game
 
   def initialize
-    @ui_score = {:hand => 0, :bout => 0, :match => 0} ## TAKE THESE OUT OF METHOD
-    @ai_score = {:hand => 0, :bout => 0, :match => 0} ## OR WILL RESET EVERY TIME
+    @ui_score = {:engagement => 0, :bout => 0, :match => 0, :game => 0}
+    @ai_score = {:engagement => 0, :bout => 0, :match => 0, :game => 0}
     @ui_point = 0
     @ai_point = 0
     puts "Hurry, the match is starting!"
@@ -31,7 +31,7 @@ class Game
 
   # USER CHOOSE
   def user_choice
-    puts "press '1' for Rock, press '2' for Paper, or press '3' for Scissors."
+    puts "Press '1' for Rock, press '2' for Paper, or press '3' for Scissors."
     @ui_choice = gets.to_i
     if @ui_choice == 1
       @user_shoot = "Rock"
@@ -43,58 +43,52 @@ class Game
       puts "Do you even know how to play this game?!"
       exit
     end
-    puts "Computer throws #{@ai_shoot}"
-    puts "#{@name} throws #{@user_shoot}"
+    puts "RO... SHAM... BO!"
+    puts "Computer throws #{@ai_shoot}....."
+    puts "#{@name} throws #{@user_shoot}!"
   end
 
   # HAND-BOUT-MATCH TALLY
   def match_tally
     ##HAND
     if @ai_point == 1
-      @ai_score[:hand] += 1
+      @ai_score[:engagement] += 1
     elsif @ui_point == 1
-      @ui_score[:hand] += 1
+      @ui_score[:engagement] += 1
     end
     ##BOUT
-    if @ui_score[:hand] == 2
-      puts "Bout: #{@name}!" #<=== change player to @player_name
+    if @ui_score[:engagement] == 2
+      puts "Bout -- #{@name}!" #<=== change player to @player_name
       # puts "Begin next bout!" #<=== call on bout? match?
-      @ui_score[:hand] = 0
+      @ui_score[:engagement] = 0
       @ui_score[:bout] += 1
-      @ai_score[:hand] = 0
+      @ai_score[:engagement] = 0
     end
-    if @ai_score[:hand] == 2
-      puts "Bout: Computer!" #<=== change player to @player_name
+    if @ai_score[:engagement] == 2
+      puts "Bout -- Computer!" #<=== change player to @player_name
       # puts "Begin next bout!" #<=== call on bout? match?
-      @ui_score[:hand] = 0
-      @ai_score[:hand] = 0
+      @ui_score[:engagement] = 0
+      @ai_score[:engagement] = 0
       @ai_score[:bout] += 1
     end
       ##MATCH
       if @ui_score[:bout] == 2
-        puts "Match: #{@name}!" #<=== change player to @player_name
+        puts "Match -- #{@name}!" #<=== change player to @player_name
         # puts "Begin next bout!" #<=== call on bout? match?
-        @ui_score[:hand] = 0
+        @ui_score[:engagement] = 0
         @ui_score[:bout] = 0
-        @ui_score[:match] += 1
-        @ai_score[:hand] = 0
+        @ai_score[:engagement] = 0
         @ai_score[:bout] = 0
+        @ui_score[:match] += 1
       elsif @ai_score[:bout] == 2
-        puts "Match: Computer!" #<=== change player to @player_name
+        puts "Match -- Computer!" #<=== change player to @player_name
         # puts "Begin next bout!" #<=== call on bout? match?
-        @ai_score[:hand] = 0
+        @ui_score[:engagement] = 0
+        @ui_score[:bout] = 0
+        @ai_score[:engagement] = 0
         @ai_score[:bout] = 0
         @ai_score[:match] += 1
-        @ui_score[:hand] = 0
-        @ui_score[:bout] = 0
     end
-    # if @ai_score[:match] == 2
-    #   puts "'player' is the winner!"
-    #   exit
-    # elsif @ui_score[:match] == 2
-    #   puts "Computer is the winner!"
-    #   exit
-    # end
   @ai_point = 0
   @ui_point = 0
   end
@@ -104,53 +98,86 @@ class Game
     case
   when @ui_choice == 1
     if @ai_choice == 2
-      puts "Paper covers Rock! Point Computer!"
+      puts "Paper covers Rock! Engagement -- Computer!"
       @ai_point += 1
     elsif @ai_choice == 3
-      puts "Rock crushes Scirrors! Point #{@name}!"
+      puts "Rock crushes Scissors! Engagement -- #{@name}!"
       @ui_point += 1
     end
   when @ui_choice == 2
     if @ai_choice == 1
-      puts "Paper covers Rock! Point #{@name}!"
+      puts "Paper covers Rock! Engagement -- #{@name}!"
       @ui_point += 1
     elsif @ai_choice == 3
-      puts "Scissors cuts Paper! Point 'Computer!'"
+      puts "Scissors cuts Paper! Engagement -- 'Computer!'"
       @ai_point += 1
     end
   when @ui_choice == 3
     if @ai_choice == 1
-      puts "Rock crushes Scissors! Point Computer!'"
+      puts "Rock crushes Scissors! Engagement -- Computer!'"
       @ai_point += 1
     elsif @ai_choice == 2
-      puts "Scissors cuts Paper! Point #{@name}!"
+      puts "Scissors cuts Paper! Engagement -- #{@name}!"
       @ui_point += 1
     end
   end
   if @ui_choice == @ai_choice
-    puts "TIE! Go again!"
+    puts "Engagement -- TIE! Go again!"
   end
-  # puts @ai_point
-  # puts @ui_point
+  # puts @ai_point  ##DEBUG
+  # puts @ui_point  ##DEBUG
   end
 
   def bout
-    while @ui_score[:match] < 1 && @ai_score[:match] < 1
+    while true
     ai_choice
     user_choice
     winner
     match_tally
-    puts "#{@name}"
-    puts "     #{@ui_score}"
-    puts "Computer"
-    puts "     #{@ai_score}"
+    puts "#{@name}:"
+    puts "     <<#{@ui_score}>>"
+    puts "Computer:"
+    puts "     <<#{@ai_score}>>"
     end
-    if @ui_score[:match] == 2
-      puts "Game! Winner: #{@name}!"
-      exit
-    elsif @ai_score[:match] == 2
+    if @ui_score[:match] == 1
+      @ui_score[:match] = 0
+      @ui_score[:game] = 1
+      puts "Game! Winner: #{@name}!!!"
+      message = %{
+       __    __  ____  ____   ____     ___  ____   __
+      |  |__|  ||    ||    \\ |    \\   /  _]|    \\ |  |
+      |  |  |  | |  | |  _  ||  _  | /  [_ |  D  )|  |
+      |  |  |  | |  | |  |  ||  |  ||    _]|    / |__|
+      |  `  '  | |  | |  |  ||  |  ||   [_ |    \\  __
+       \\      /  |  | |  |  ||  |  ||     ||  .  \\|  |
+        \\_/\\_/  |____||__|__||__|__||_____||__|\\_||__|
+      }
+      puts message
+    elsif @ai_score[:match] == 1
+      @ui_score[:match] = 0
+      @ui_score[:game] = 1
       puts "Game! Winner: Computer!"
-      exit
+    end
+    play_again
+  end
+
+  def play_again
+    puts "Would you like to play again?"
+    puts "Press '1' to play again or any other key to exit."
+    replay = gets.to_i
+    if replay == 1
+      bout
+    else
+      puts "Are you *SURE* you want to quit?"
+      puts "Press '1' to exit or any other key to play again."
+      quit = gets.to_i
+      if quit == 1
+        puts "Are you sure you're sure...?"
+        yes = gets
+        exit
+      else
+        bout
+      end
     end
   end
 end
